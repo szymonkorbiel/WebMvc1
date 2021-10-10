248d3ce3e30c049c9c415f57d2a5a11bed9867ab
szymon.korbiel <szymon.korbiel@PK1-17>
2021-10-10 09:19:02 +0200

    Add project files.

---
+++ WebMvc1/Models/FormModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebMvc1.Models
+{
+    public class FormModel
+    {
+        [Display(Name = "Nazwa elementu")]
+        public string Name { get; set; }
+        [Display(Name = "Opis elementu")]
+        public string Description { get; set; }
+        [Display(Name = "Element widoczny?")]
+        public bool IsVisible { get; set; }
+    }
+}
